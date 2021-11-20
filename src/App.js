362f987bcbb5199e64e77a44218ec154849f026f
Babu Aravind Sivamani <itsbabuaravind@gmail.com>
2021-11-19 21:15:41 -0500

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/projects/:id" element={<Project />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,46 +1,39 @@
-import {
-  createContext,
-  useREducer,
-  useEffect,
-} from "core-js/library/fn/reflect/es7/metadata";
+import { createContext, useReducer, useEffect } from "react";
 import { auth_ } from "../firebase/config";
 
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
-  switch ((action, type)) {
+  switch (action.type) {
     case "LOGIN":
       return { ...state, user: action.payload };
-
     case "LOGOUT":
       return { ...state, user: null };
-
-    case AUTH_IS_READY:
+    case "AUTH_IS_READY":
       return { user: action.payload, authIsReady: true };
-
     default:
       return state;
   }
 };
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = userReducer(authReducer, {
+  const [state, dispatch] = useReducer(authReducer, {
     user: null,
     authIsReady: false,
   });
+
+  useEffect(() => {
+    const unsub = auth_.onAuthStateChanged((user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
+    });
+  }, []);
+
+  console.log("AuthContext state:", state);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
-
-useEffect(() => {
-  const unsub = projectAuth.onAuthStateChanged((user) => {
-    dispatch({ type: "AUTH_ISREADY", payload: user });
-    unsub();
-  });
-}, []);
-
-console.log("AuthContext state", state);
-
-return (
-  <AuthContext.Provider value={{ ...state, dispatch }}>
-    {children}
-  </AuthContext.Provider>
-);
